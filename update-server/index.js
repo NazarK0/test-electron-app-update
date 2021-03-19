155d24142ca96d5a217541692a2f91dc3a2e288d
lexux14 Hrigorov <hansolhrigorov@gmail.com>
2021-03-19 14:13:45 +0200

    update win

--- update-server/index.js
+++ update-server/index.js
@@ -4,25 +4,26 @@ const path = require('path')
 
 const app = express()
 
-app.get('/updates/latest', function (req, res) {
+app.get('/updates/:platform/latest', function (req, res) {
     const latest = getLatestRelease()
     const clientVersion = req.query.v
     if (clientVersion === latest) {
         res.status(204).end()
     } else {
         let baseURL = getBaseUrl()
-        let updateURL = baseURL + '/releases/darwin/' + latest + '/electron.zip'
+        let updateURL = baseURL + `/releases/${patform}/${latest}/electron.zip`;
+
         res.json({
             url: updateURL,
             name: "My Release Name",
             notes: "These are some release notes",
-            pub_date: "2017-04-18T12:29:53+01:00"
+            pub_date: (new Date()).toISOString()
         })
     }
 })
 
-let getLatestRelease = () => {
-    const dir = __dirname + '/releases/darwin'
+let getLatestRelease = (platform) => {
+    const dir = __dirname + `/releases/${platform}`;
     const versionsDesc = fs.readdirSync(dir).filter((file) => {
         const filePath = path.join(dir, file)
         return fs.statSync(filePath).isDirectory()
